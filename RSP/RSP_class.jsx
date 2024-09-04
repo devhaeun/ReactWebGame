b43591b2df5915939b1172f98c2e14c02037cc4c
devhaeun <lala819010@gmail.com>
2024-09-05 00:08:48 +0900

    가위바위보: 웹팩/핫리로딩 설정

---
+++ RSP/RSP_class.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+const { Component } = React;
+
+class RSP extends Component {
+    state = {};
+    render() {
+        return (
+            <>
+                <div>가위바위보 게임</div>
+            </>
+        );
+    }
+}
+
+module.exports = RSP;
---
+++ RSP/client.jsx
@@ -0,0 +1,6 @@
+const React = require('react');
+const ReactDOM = require('react-dom/client');
+
+const RSP = require('./RSP_class');
+
+ReactDOM.createRoot(document.querySelector('#root')).render(<RSP />);
