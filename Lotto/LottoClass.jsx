bb9963bcbbc30910e0ced35ac4f92657c7216ce0
devhaeun <lala819010@gmail.com>
2024-09-06 00:45:05 +0900

    로또: 클래스 컴포넌트 구현

--- Lotto/LottoClass.jsx
+++ Lotto/LottoClass.jsx
@@ -1,18 +1,89 @@
 const React = require('react');
 const { Component } = React;
+const Ball = require('./Ball')
+
+function getWinNumbers() {
+    console.log('getWinNumbers');
+    const candidate = Array(45).fill().map((v,i) => i+1);
+    const shuffle = [];
+    while (candidate.length>0) {
+        shuffle.push(candidate.splice(Math.floor(Math.random()*candidate.length), 1)[0]);
+    }
+    const bonusNumber = shuffle[shuffle.length-1];
+    const winNumbers = shuffle.slice(0,6).sort((p,c) => p-c);
+    return [...winNumbers, bonusNumber];
+}
 
 class Lotto extends Component {
     state = {
-        winNumbers: [],
+        winNumbers: getWinNumbers(),
         winBalls: [],
         bonus: null,
         redo: false,
     };
 
+    timeouts = [];
+
+    runTimeouts = () => {
+        console.log('runTimeouts');
+        const { winNumbers } = this.state;
+        for (let i=0; i<winNumbers.length-1; i++) {
+            this.timeouts[i] = setTimeout(() => {
+                this.setState((prevState) => {
+                    return {
+                        winBalls: [...prevState.winBalls, winNumbers[i]],
+                    };
+                });
+            }, 1000*(i+1));
+        }
+        this.timeouts[6] = setTimeout(() => {
+            this.setState({
+                bonus: winNumbers[6],
+                redo: true,
+            });
+        }, 7000);
+    };
+
+    componentDidMount() {
+        console.log('componentDidMount');
+        this.runTimeouts();
+        console.log('로또 숫자 생성');
+    }
+    
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.winBalls.length === 0) {
+            this.runTimeouts();
+        }
+    }
+
+    componentWillUnmount() {
+        this.timeouts.forEach((v) => {
+            clearTimeout(v);
+        });
+    }
+
+    onClickRedo = () => {
+        console.log('onClickRedo');
+        this.setState({
+            winNumbers: getWinNumbers(),
+            winBalls: [],
+            bonus: null,
+            redo: false,
+        });
+        this.timeouts = [];
+    };
+
     render() {
+        const { winBalls, bonus, redo } = this.state;
         return (
             <>
                 <div>오늘의 로또 당첨 번호는?</div>
+                <div id="result">
+                    {winBalls.map((v) => <Ball key={v} number={v} />)}
+                </div>
+                <div>보너스!</div>
+                {bonus && <Ball number={bonus} />}
+                {redo && <button onClick={this.onClickRedo}>한 번 더!</button>}
             </>
         );
     };
