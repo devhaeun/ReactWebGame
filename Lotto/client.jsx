01de764fa7e17fefdce81c210df0083d023e6472
devhaeun <lala819010@gmail.com>
2024-09-06 00:59:44 +0900

    로또: Hooks - useMemo 이용

--- Lotto/client.jsx
+++ Lotto/client.jsx
@@ -1,6 +1,6 @@
 const React = require('react');
 const ReactDOM = require('react-dom/client');
 
-const Lotto = require('./LottoClass');
+const Lotto = require('./Lotto');
 
 ReactDOM.createRoot(document.querySelector('#root')).render(<Lotto />);
